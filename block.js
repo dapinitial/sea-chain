3af01e45fb488c8ae415ddbce9923673a8369b2f
David Puerto <me@davidpuerto.com>
2018-04-08 16:11:20 -0700

    Block class created

---
+++ block.js
@@ -0,0 +1,26 @@
+class Block {
+  constructor(timestamp, lastHash, hash, data) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  toString() {
+    return `Block - 
+      Timestamp: ${this
+      .timestamp
+      .substring(0, 10)}
+      Last Hash: ${this
+      .lastHash
+      .substring(0, 10)}
+      Hash     : ${this
+      .hash
+      .substring(0, 10)}
+      Data     : ${this
+      .data
+      .substring(0, 10)}`;
+  }
+}
+
+module.exports = Block;
