0fea860aadf6565693678a81b712cb92e3f4d773
David Puerto <me@davidpuerto.com>
2018-06-01 23:56:30 -0700

    synchronizing the blockchain

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -36,6 +36,30 @@ class P2pServer {
       .sockets
       .push(socket);
     console.log('Socket connected');
+
+    this.messageHandler(socket);
+    this.sendChain(socket);
+  }
+
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+
+      this
+        .blockchain
+        .replaceChain(data);
+    });
+  }
+
+  sendChain(socket) {
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  syncChains() {
+    this
+      .sockets
+      .forEach(socket => this.sendChain(socket));
   }
 }
 
